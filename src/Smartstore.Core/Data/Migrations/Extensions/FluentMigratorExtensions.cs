e463a08b1c1991a0eee8185f19842272918e50f6
Murat Cakir <muratc@smartstore.de>
2026-03-26 00:50:49 +0100

    FluentMigrator AsJson() column extension

--- src/Smartstore.Core/Data/Migrations/Extensions/FluentMigratorExtensions.cs
+++ src/Smartstore.Core/Data/Migrations/Extensions/FluentMigratorExtensions.cs
@@ -1,4 +1,7 @@
-﻿using FluentMigrator.Builders.Create.Table;
+﻿using FluentMigrator.Infrastructure;
+using FluentMigrator.Builders;
+using FluentMigrator.Builders.Create.Table;
+using Smartstore.Data;
 
 namespace FluentMigrator
 {
@@ -6,12 +9,28 @@ namespace FluentMigrator
     {
         public static ICreateTableWithColumnSyntax WithIdColumn(this ICreateTableWithColumnSyntax root)
         {
-            return root.WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable();
+            return root.WithColumn("Id")
+                .AsInt32()
+                .PrimaryKey()
+                .Identity()
+                .NotNullable();
         }
 
-        public static ICreateTableColumnOptionOrWithColumnSyntax AsMaxString(this ICreateTableColumnAsTypeSyntax root)
+        public static TNext AsMaxString<TNext>(this IColumnTypeSyntax<TNext> root)
+            where TNext : IFluentSyntax
         {
             return root.AsString(int.MaxValue);
+        }
+
+        public static TNext AsJson<TNext>(this IColumnTypeSyntax<TNext> root)
+            where TNext : IFluentSyntax
+        {
+            return (DataSettings.Instance.DbFactory?.DbSystem) switch
+            {
+                DbSystemType.MySql      => root.AsCustom("JSON"),
+                DbSystemType.PostgreSql => root.AsCustom("JSONB"),
+                _                       => root.AsString(int.MaxValue)  // SQL Server: NVARCHAR(MAX), SQLite: TEXT
+            };
         }
     }
 }
